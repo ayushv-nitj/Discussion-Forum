6fcf21035bac38a2ee66c6ad4ff3b9d9c1353c9e
ayushv-nitj <ayush7610jsr@gmail.com>
2025-07-13 19:21:40 +0530

    update notifications feature

--- backend/models/MessageRead.js
+++ backend/models/MessageRead.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
-const MessageReadSchema = new mongoose.Schema({
-  threadId: { type: String, required: true },
-  messageId: { type: String, required: true },
-  userId: { type: String, required: true },
-  readAt: { type: Date, default: Date.now }
+const messageReadSchema = new mongoose.Schema({
+  threadId: {
+    type: String,
+    required: true,
+  },
+  messageId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Message',
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  readAt: {
+    type: Date,
+    default: Date.now,
+  }
 });
 
-module.exports = mongoose.model("MessageRead", MessageReadSchema);
+module.exports = mongoose.model('MessageRead', messageReadSchema);
